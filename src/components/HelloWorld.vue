ca488d4f33dbacb9c8e05421944477b66758067f
fusong <yufusong@foxmail.com>
2019-09-07 18:45:35 +0800

    'init'

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,90 @@
+<template>
+  <div style="height: auto;background-color: #42b983">
+    <button @click="click" style="margin-right: 20px" >我要开会</button>
+    <button @click="click">会议记录</button>
+
+  </div>
+</template>
+
+<script>
+
+  import * as dd from 'dingtalk-jsapi'
+import axios from 'axios';
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  },
+  data() {
+    return {
+      message: '22222',
+      ggg: [],
+      params: {
+        curentPage: 1,
+      },
+      result: {},
+    };
+  },
+  created: function () {
+    console.log("======>>bg")
+    dd.runtime.permission.requestAuthCode({
+      corpId: "ding251335d31062a7f535c2f4657eb6378f",
+      onSuccess: function(result) {
+        dd.device.notification.alert({
+          message: "success" + result + result.code +"_"+ result[1],
+          title: "提示",
+          buttonName: "收到",
+          onSuccess : function() {
+
+          },
+          onFail : function(err) {}
+        });
+      },
+      onFail : function(err) {
+        alert("error");
+      }
+
+    });
+    console.log("======>>ed")
+  },
+  methods: {
+    click() {
+      alert(33333);
+      axios.get('url/ttt', {  params: this.params })
+              .then((res) => {
+               /*console.log(res);*/
+                this.result = res;
+              }, function () {
+               /* console.log(err);*/
+              });
+    },
+    gggg() {},
+  },
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+  button {
+    margin-top: 8px;
+    width: 200px;
+    height: 80px;
+    font-size: 18px;
+    border-radius: 18px;
+    border: 1px solid grey;
+  }
+</style>
---
+++ vue.config.js
@@ -0,0 +1,29 @@
+/*
+const path = require('path')
+
+// eslint-disable-next-line no-unused-vars
+
+function resolve(dir) {
+  return path.join(__dirname, '.', dir);
+}
+module.exports = {
+  css: {
+    sourceMap: true,
+  },
+  productionSourceMap: false,
+  assetsDir: 'static',
+  publicPath: './',
+  devServer: {
+    host: 'dd.szlcsc.com',
+    port: 443,
+    open: false,
+    public: 'dd.szlcsc.com',
+  },
+  configureWebpack: (config) => {
+    if (process.env.NODE_ENV !== 'production') {
+      config.devtool = 'source-map';
+    }
+  },
+};
+
+*/
